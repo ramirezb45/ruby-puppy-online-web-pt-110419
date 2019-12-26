393f6aae523d3012acfd6dba6430b7b6151b70d7
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-12-26 03:17:10 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
-# Add your code here
+     def initialize(name)
+    @name = name
+    @@all << self
+    save
+  end
+
+  def self.all
+@@ -20,4 +20,8 @@ def self.print_all
+  def self.clear_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+end
